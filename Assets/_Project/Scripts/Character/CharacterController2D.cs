f4e8e0f054f4cc3a25af89f1e82c82822df39177
yucelz <yucelz@gmail.com>
2024-01-07 21:00:38 +0000

    ++

--- Assets/_Project/Scripts/Character/CharacterController2D.cs
+++ Assets/_Project/Scripts/Character/CharacterController2D.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterController : MonoBehaviour
+public class CharacterController2D : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
